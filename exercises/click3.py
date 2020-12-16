b5d236a0c64876e7e61400cbec55f1becd1b7cad
Hugo <hugo.nazac@ens.uvsq.fr>
2020-12-16 16:25:56 +0100

    ok

---
+++ exercises/click3.py
@@ -0,0 +1,27 @@
+import tkinter as tk 
+
+
+nb_clic = 0
+point = (0,0)
+
+
+def draw_line(event) : 
+    global nb_clic, point
+    if nb_clic == 0: # première fois où l'on clique
+        nb_clic = 1
+        point = (event.x, event.y)
+    else:
+        nb_clic = 0
+        if (250 - event.x) * (250 - point[0]) >= 0: # du même côté
+            color = "blue"
+        else:
+            color = "red"
+        canvas.create_line(point, (event.x, event.y), fill=color)
+
+
+racine = tk.Tk()
+canvas = tk.Canvas(racine, width=500, height=500, bg="black")
+canvas.create_line((250,0), (250,500), fill="white")
+canvas.bind("<Button-1>", draw_line)
+canvas.grid()
+racine.mainloop()
